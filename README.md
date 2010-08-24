d844cf263e4b61b3c38974e3e5d627b706052b5d
Daniel Shaw <github@dshaw.com>
2010-08-24 12:22:20 -0400

    Added README. Minor UI tweaks. Moved port from 8080 to 8888. (8080 port conflicts on my machine.)

---
+++ README.md
@@ -0,0 +1,18 @@
+# twitter-steam-proxy #
+
+This is based on a hack posted by @cramforce.
+
+@dshaw
+
+
+## Setup ##
+
+* npm install socket.io 
+* npm install node-static 
+* REPL node> (new Buffer("twitterusername:password")).toString("base64"); 
+* cd lib/
+* node server.js <AuthHeaderValue>
+* http://localhost:8888
+* Now post anything, favorite, etc. Comes up almost before the UI finishes processing.
+
+
--- public/client.js
+++ public/client.js
@@ -1,19 +1,17 @@
 (function () {
-
   var ol = document.createElement("ol");
-  document.body.appendChild(ol)
-  
+  document.body.appendChild(ol);
 
   io.setPath('/socket.io/');
-  var socket = new io.Socket('localhost', {
-    port: 8080
-  });
+  var socket = new io.Socket('localhost', { port: 8888 });
   socket.connect();
-  socket.send('some data');
+  socket.send('client connect');
   socket.on('message', function(data) {
     data = JSON.parse(data);
+    // console.log(data);
     var li = document.createElement("li");
     li.textContent = data.text || JSON.stringify(data, null, " ");
     ol.appendChild(li);
+    li.focus();
   });
 })();
