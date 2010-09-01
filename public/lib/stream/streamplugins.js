6fed1c418459f595cc6d3ca15b5d7227d13ce466
antimatter15 <antimatter15@gmail.com>
2010-09-01 19:26:04 -0400

    changed whitespace

--- public/lib/stream/streamplugins.js
+++ public/lib/stream/streamplugins.js
@@ -194,10 +194,10 @@ require.def("stream/streamplugins",
           
           //from http://gist.github.com/492947 and http://daringfireball.net/2010/07/improved_regex_for_matching_urls
           var GRUBERS_URL_RE = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/ig;
-          
+
           text = text.replace(GRUBERS_URL_RE, function(url){
-						return '<a href="'+((/^\w+\:\//.test(url)?'':'http://')+url)+'">'+url+'</a>';
-					})
+            return '<a href="'+((/^\w+\:\//.test(url)?'':'http://')+url)+'">'+url+'</a>';
+          })
 					
           // screen names
           text = text.replace(/(^|\W)\@([a-zA-Z0-9_]+)/g, function (all, pre, name) {
