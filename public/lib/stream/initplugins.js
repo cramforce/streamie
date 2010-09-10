52b492b51471456089d85d8835bd0f0ebdf94c1a
Malte Ubl <malubl@Joose.local>
2010-09-10 23:34:05 +0200

    Also display sent direct messages on prefill

--- public/lib/stream/initplugins.js
+++ public/lib/stream/initplugins.js
@@ -252,7 +252,7 @@ require.def("stream/initplugins",
               if(status == "success") {
                 all = all.concat(tweets)
               };
-              if(returns == 5) { // all four APIs returned, we can start drawing
+              if(returns == 6) { // all four APIs returned, we can start drawing
                 var seen = {};
                 all = all.filter(function (tweet) { // filter out dupes
                   var ret = !seen[tweet.id];
@@ -296,6 +296,7 @@ require.def("stream/initplugins",
             rest.get("/1/statuses/friends_timeline.json?count=100", handleSince);
             rest.get("/1/favorites.json", handle);
             rest.get("/1/direct_messages.json", handle)
+            rest.get("/1/direct_messages/sent.json", handle)
           }
           
           $(document).bind("awake", function (e, duration) { // when we awake, we might have lost some tweets
