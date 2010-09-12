95df304ac41e7d64d65752c68239b2dcd50de232
Malte Ubl <malubl@Joose.local>
2010-09-12 20:19:42 +0200

    Make uploads work in main status. Close modal by hitting escape

--- public/lib/stream/popin.js
+++ public/lib/stream/popin.js
@@ -21,6 +21,15 @@ require.def("stream/popin",
       $("#overlay").hide();
     }
     
+    var current;
+    
+    $(document).bind("key:escape", function (e) {
+      if(current) {
+        current.trigger("close");
+        return false;
+      }
+    });
+    
     return {
       // show a popin from the given id
       show: function (id, options) { // options.modal
@@ -39,13 +48,16 @@ require.def("stream/popin",
         ele.bind("close", function (e) {
           hideModal();
           ele.removeClass("show");
+          current = null;
         });
         
         if(options.modal) {
           makeModal(ele);
+          current = ele;
         }
         
         ele.addClass("show");
+        ele.focus();
         
         return {
           close: function () {
