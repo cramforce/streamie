daf7e081874dfb467624a3cb62ebd4663f9c140e
Malte Ubl <malte.ubl@gmail.com>
2010-10-09 16:45:47 +0200

    Make current socket.io version happy

--- public/lib/stream/client.js
+++ public/lib/stream/client.js
@@ -30,7 +30,6 @@ require.def("stream/client",
     }
     
     function connect (cb) {
-      io.setPath('/ext/socket.io/');
       window.WEB_SOCKET_SWF_LOCATION = "/foobar"; // we do not use flash, but socket.io still complaints
       var socket = new io.Socket(location.hostname, { 
         port: location.port || 80,
