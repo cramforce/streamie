c8cd03e6a3d3ebda787d1b0968beca79afd5a049
Andreas Blixt <andreas@blixt.org>
2010-11-04 14:04:38 +0100

    Fixed spelling mistake prefered -> preferred. Did not change settings keys since I don't know what effect that would have to users who already have the old key.

--- public/lib/stream/streamplugins.js
+++ public/lib/stream/streamplugins.js
@@ -13,7 +13,7 @@ require.def("stream/streamplugins",
     settings.registerNamespace("stream", "Stream");
     settings.registerKey("stream", "showRetweets", "Show Retweets",  true);
     settings.registerKey("stream", "keepScrollState", "Keep scroll level when new tweets come in",  true); 
-    settings.registerKey("stream", "translate", "Automatically translate to your prefered language", false ); 
+    settings.registerKey("stream", "translate", "Automatically translate to your preferred language", false ); 
 
     // convert google.language.Languages list of support languages to a settings values
     var translateValues  = {};
@@ -22,7 +22,7 @@ require.def("stream/streamplugins",
       if( codeLang.length == 0 ) continue;
       translateValues[codeLang] = humanLang.charAt(0).toUpperCase() + humanLang.substring(1).toLowerCase();
     }
-    settings.registerKey("stream", "preferedLanguage", "Prefered language", "en", translateValues ); 
+    settings.registerKey("stream", "preferedLanguage", "Preferred language", "en", translateValues ); 
     
     var template = _.template(templateText);
     
