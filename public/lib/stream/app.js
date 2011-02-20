605b7183b80f0936d7bc0f91f117d6a9b56a0e2b
Malte Ubl <malte.ubl@gmail.com>
2011-02-20 16:34:12 +0100

    Include screen name in active use tracking

--- public/lib/stream/app.js
+++ public/lib/stream/app.js
@@ -121,7 +121,7 @@ require.def("stream/app",
                   plugin.func.call(function () {}, stream, plugin);
                 });
                 $(document).trigger("streamie:init:complete");
-                $(document).trigger("streamie:activeuse");
+                $(document).trigger("streamie:activeuse", [stream.user.screen_name]);
               }
             }
             else if(data.tweet) {
